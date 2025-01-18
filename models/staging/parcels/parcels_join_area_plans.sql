ee3bf3e30eff6fd14d35999fe7b4fc6f005c8238
bnoffke <bnoffke3790@gmail.com>
2025-01-18 15:49:12 -0600

    Adding macro docs and some cleanup

--- models/staging/parcels/parcels_join_area_plans.sql
+++ models/staging/parcels/parcels_join_area_plans.sql
@@ -6,17 +6,3 @@
 
 
 {{ apply_overlay_to_parcels('area_plans_localize_geom','area_plans','area_plan')}}
-/*
-select
-
-    parcels.parcel_id,
-    parcels.parcel_year,
-    area_plans.area_plan,
-    ST_Area(ST_Intersection(parcels.geom_4326,area_plans.geom_4326)) as intersect_area,
-    row_number() over (partition by parcels.parcel_id,parcels.parcel_year order by ST_Area(ST_Intersection(parcels.geom_4326,area_plans.geom_4326)) desc) as intersect_rank
-
-from {{ ref('parcels_fix_condos') }} parcels
-
-inner join {{ ref('area_plans_localize_geom') }} area_plans
-    on ST_Intersects(parcels.geom_4326,area_plans.geom_4326)
-*/
