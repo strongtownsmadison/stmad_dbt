2bac5d89a5a2cffd4f1544686dc7d3465deedc2b
bnoffke <bnoffk3790@gmail.com>
2025-09-20 11:33:09 -0500

    Order of operations fix

--- dbt_start_dev.sh
+++ dbt_start_dev.sh
@@ -4,13 +4,13 @@ echo "✅ Virtual environment activated"
 
 #Prep prod's current state for defer
 mkdir -p state
+export DBT_DEFER=1
+export DBT_STATE=./state
 dbt compile --target prod
 cp target/manifest.json state/
 echo "✅ DBT state prepared for defer"
 
 #Set env vars for defer
-export DBT_DEFER=1
-export DBT_STATE=./state
 echo "✅ DBT_DEFER and DBT_STATE environment variables set"
 
 echo "🎉 DBT development environment is ready!"
